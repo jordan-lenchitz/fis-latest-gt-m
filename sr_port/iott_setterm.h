cd0536f1e761316a231cb97dd840cf71e6764997
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-05-25 14:50:11 +0000

    feat: optimize initial system

---
+++ sr_port/iott_setterm.h
@@ -0,0 +1,19 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2001-2023 Fidelity National Information	*
+ * Services, Inc. and/or its subsidiaries. All rights reserved.	*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#ifndef SETTERM_INCLUDED
+#define SETTERM_INCLUDED
+
+void iott_setterm(io_desc *iod);
+void iott_resetterm(io_desc *iod);
+
+#endif /* SETTERM_INCLUDED */
