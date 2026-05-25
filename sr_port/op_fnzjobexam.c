aa78ee157cce5a97abdc76228663cf16db0529cc
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-05-25 14:50:40 +0000

    feat: sync basic core

---
+++ sr_port/op_fnzjobexam.c
@@ -0,0 +1,23 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2001-2022 Fidelity National Information	*
+ * Services, Inc. and/or its subsidiaries. All rights reserved.	*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "mdef.h"
+#include "op.h"
+#include "jobexam_process.h"
+
+void op_fnzjobexam(mval *prelimSpec, mval *fmt, mval *finalSpec)
+{
+	MV_FORCE_STR(prelimSpec);
+	MV_FORCE_STR(fmt);
+	jobexam_process(prelimSpec, finalSpec, fmt);
+	return;
+}
